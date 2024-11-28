5ee3e7301306cc9c2a2720afa6276fb89b6bc339
Ori Korngut <ori.korngut@gmail.com>
2024-11-28 14:43:22 +0200

    Added some API

--- src/Baby.java
+++ src/Baby.java
@@ -1,3 +1,10 @@
+/*
+This class represents a Baby object.
+
+@version 2025a
+@author Ori Korngut
+ */
+
 public class Baby {
     private String _firstName;
     private String _lastName;
@@ -10,6 +17,18 @@ public class Baby {
     private static final int MIN_DAY_IN_YEAR = 1;
     private static final int MAX_DAY_IN_YEAR = 365;
 
+    /*
+    Baby constructor - If the given id and birthWeightInGrams are valid - creates a new Baby object with the parameters
+    otherwise, if the id is not valid creates the Baby with id = "000000000" and all other parameters.
+
+    @param fname - the first name of the baby.
+    @param lname - the last name of the baby.
+    @param id - the id of the baby (9 characters).
+    @param day - the day of the baby's birth.
+    @param month - the month of the baby's birth.
+    @param year - the year of the baby's birth.
+    @param birthWeightInGrams - the weight of the baby at birth in grams (should be minimum 1KG).
+     */
     public Baby (String fName, String lName, String id, int day, int month, int year, int birthWeightInGrams){
         this._firstName = fName;
         this._lastName = lName;
@@ -26,6 +45,11 @@ public class Baby {
         this._currentWeight = new Weight(birthWeightInGrams);
     }
 
+    /*
+    Copy constructor:
+
+    @param other - the baby to be copied.
+     */
     public Baby (Baby other){
         this._firstName = other._firstName;
         this._lastName = other._lastName;
@@ -41,6 +65,11 @@ public class Baby {
         }
     }
 
+    /*
+    Gets the first name.
+
+    @return the first name of this baby.
+     */
     public String getFirstName() {
         return _firstName;
     }
@@ -57,6 +86,9 @@ public class Baby {
         return _id;
     }
 
+    /*
+    Gets the birth weight.
+     */
     public Weight getBirthWeight() {
         return _birthWeight;
     }
@@ -75,6 +107,9 @@ public class Baby {
         );
     }
 
+    /*
+    Checks if two babies are twins.
+     */
     public boolean areTwins (Baby other) {
         return other._lastName.equals(this._lastName) &&
                 !other._firstName.equals(this._firstName) &&
@@ -82,6 +117,9 @@ public class Baby {
                 (other._dateOfBirth == this._dateOfBirth || other._dateOfBirth == this._dateOfBirth.tomorrow() || other._dateOfBirth.tomorrow() == this._dateOfBirth);
     }
 
+    /*
+    Checks if two babies are the same.
+     */
     public boolean equals (Baby other) {
         return other._lastName.equals(this._lastName) &&
                 other._firstName.equals(this._firstName) &&
